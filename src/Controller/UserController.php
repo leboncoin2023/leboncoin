0b55541a0700b15dc41eb5c6b00eae6816042715
Drahc0r <132657494+Drahc0r@users.noreply.github.com>
2023-07-13 10:59:29 +0200

    modif, user controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,10 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    
+    #[Route('/', name: 'app_user')]
     public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/edit', name: 'app_user_edit')]
+    public function edit(): Response
+    {
+        return $this->render('user/edit.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/delete', name: 'app_user_delete')]
+    public function delete(): Response
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
