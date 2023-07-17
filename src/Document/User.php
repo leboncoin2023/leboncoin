847e4a3f2d10eb0a9a602512cd584333958ab916
Drahc0r <132657494+Drahc0r@users.noreply.github.com>
2023-07-17 09:54:27 +0200

    Création du document user

--- src/Document/User.php
+++ src/Document/User.php
@@ -15,6 +15,12 @@ class User
     #[MongoDB\Field(type: 'string')]
     private string $username;
 
+    #[MongoDB\Field(type: 'string')]
+    private string $email;
+
+    #[MongoDB\Field(type: 'string')]
+    private string $password;
+
     
 
 //GET____________________________________
@@ -24,26 +30,47 @@ class User
         return $this->id;
     }
 
-    public function getname(): string
+    public function getUserName(): string
     {
 
         return $this->username;
+    }
+
+    public function getEmail(): string
+    {
+
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+
+        return $this->password;
     }
 
     
 
 
 //SET____________________________________
-    public function setname(string $name)
+    public function setUserName(string $name)
     {
 
         $this->username = $name;
         return $this;
     }
 
-    
+    public function setEmail(string $email)
+    {
 
+        $this->email = $email;
+        return $this;
+    }
 
+    public function setPassword(string $password)
+    {
 
+        $this->password = $password;
+        return $this;
+    }
 
 }
