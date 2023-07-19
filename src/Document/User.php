39ffc3579ffd07bd8cc3f4dc6c3c563c67abadeb
aynsl <132562728+aynsl@users.noreply.github.com>
2023-07-19 13:51:17 +0200

    modification tableau de collection user

--- src/Document/User.php
+++ src/Document/User.php
@@ -33,7 +33,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     //#[MongoDB\Field(type: 'array')]
     #[MongoDB\Field(type: "collection")]
-    private array $roles ;
+    private array $roles = [] ;
 
     #[MongoDB\Field(type: 'string')]
     private string $phone;
@@ -71,7 +71,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getRoles(): array
     {
-        $roles = $this->roles;
+       $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
