e8ede8691f43120541151beb6a7f73057a17e8ea
gazario <gazario@gmail.com>
2023-07-21 17:13:49 +0200

    finalisation affichage carte de livraison

--- public/assets/js/maps.js
+++ public/assets/js/maps.js
@@ -1,20 +1,32 @@
     
     let address_input = document.getElementById('address_input');
     let search_btn = document.getElementById('search');
-    let lat="";
-    let lon="";
+    let maps_address = document.getElementById('maCarte');
+    //let lat="48.830624";
+    //let lon="2.380269";
+
+
+    //showMap("48.830624","2.380269");
 
     
         
     // Affichage et initialisation de la carte 
-    let carte = L.map('maCarte').setView([48.830624, 2.380269], 13);
+    function showMap(lat, lon){
+        let carte = L.map('maCarte').setView([lat, lon], 18);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-        //lien vers la source des données
-        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-        minZoom: 1,
-        maxZoom: 20
-    }).addTo(carte);
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+            //lien vers la source des données
+            attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+            minZoom: 1,
+            maxZoom: 20
+        }).addTo(carte);
+
+        //maps_address.innerHTML =`<i class="fa-solid fa-location-dot" style="color: #ff9200;"></i>`;
+        //let marker = L.marker(lat, lon).addTo(carte);
+        //marker.bindPopup("house");
+
+    }
+
 
 
 
@@ -24,32 +36,19 @@
         window.fetch('https://api-adresse.data.gouv.fr/search/?q=' + address_input.value)
         .then(response => response.json())
         .then(results => {
-           // for (let i=0 ;i<results.features.length;i++){
+          
                 lat = results.features[0].geometry.coordinates[1];
                 lon = results.features[0].geometry.coordinates[0];
-
-                majCarte(lat, lon);
                 console.log(lat, lon);
+                showMap(lat,lon);
+                
+                
+                
             }, 
         )
         
-        
-     
     }
 
-    function majCarte(x, y){
-
-        carte = L.map('maCarte').setView([x, y], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-            //lien vers la source des données
-            attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-            minZoom: 1,
-            maxZoom: 20
-        }).addTo(carte);
-
-        let marker = L.marker(x, y).addTo(carte);
-        marker.bindPopup(Adresse);
-    }
     
 
 
