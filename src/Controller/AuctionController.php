745efa039d4a39db43b16fbd7fde3f02b827066e
gazario <gazario@gmail.com>
2023-07-24 14:29:10 +0200

    ljuhgv

--- src/Controller/AuctionController.php
+++ src/Controller/AuctionController.php
@@ -290,10 +290,30 @@ class AuctionController extends AbstractController
 //         $dm->flush();
 //     }
 
+    #[Route('/saveform', name: 'app_auction_saveform')]
+    public function fAuction(Request $request, DocumentManager $dm ): Response {
+    $id = $request->get('id');
+    $dauction = $dm->getRepository(Auctions::class)->find($id);
+
+    // Vérifiez si l'objet "Auctions" a été trouvé
+    if(!$dauction){
+    throw $this->createNotFoundException('Auction not found for ID: ' . $id);
+    }
+    dump($dauction);
+    $form = $this->createForm(AuctionType::class, $dauction);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+    // Sauvegardez l'entité Auctions en base de données
+    $dm->persist($dauction);
+    $dm->flush();
+    }
+
     return $this->render('auction/detail.html.twig', [
-        'dauction' => $dauction,
-        'form' => $form->createView(),
+    'dauction' => $dauction,
+    'form' => $form->createView(),
     ]);
+    }
  }
 
     
