321dd946665df9ec33e7bdfe83d4aba799b60869
gazario <gazario@gmail.com>
2023-07-25 16:25:38 +0200

    finalisation page category

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,21 +1,21 @@
 
 const swiper = new Swiper(".mySwiper",{
         // Initialise un nouveau carrousel Swiper en utilisant la classe "mySwiper" comme sélecteur
-      effect:"coverflow",// Utilise l'effet de coverflow pour la transition entre les diapositives
+      /*effect:"coverflow",// Utilise l'effet de coverflow pour la transition entre les diapositives*/
       grabCursor:true,// Change le curseur en une main lorsqu'il survole le carrousel
       centeredSlider:true,// Centre la diapositive active dans le carrousel
       loop:true,//Permet de faire une boucle infinie du carrousel
 
-
+/*
  coverflowEffect:{
-      rotate:60,// L'angle de rotation des diapositives en mode coverflow
+      rotate:0,// L'angle de rotation des diapositives en mode coverflow
       stretch:0,// L'espacement horizontal entre les diapositives en mode coverflow
       slidesshadow:true // Active les ombres portées pour les diapositives en mode coverflow
-  },
+  },*/
 
   slidesPerView:"auto",// Définit le nombre de diapositives visibles par vue, "auto" signifie autant de diapositives que possible
    
-  pagination:{
+  /*pagination:{
       el:".swiper-pagination"// Sélectionne l'élément avec la classe "swiper-pagination" pour afficher la pagination
-  }   
+  }   */
 })
