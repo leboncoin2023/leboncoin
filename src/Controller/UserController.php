b5582ef17edf9be020959ef5ed1e4fcba41471da
Belfegore44 <132562896+Belfegore44@users.noreply.github.com>
2023-07-25 09:32:08 +0200

    fin de fomation 25/07
    dsdsd

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,18 +9,29 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/user')]
 class UserController extends AbstractController
 {
-    
     #[Route('/', name: 'app_user')]
     public function index(): Response
     {
+        // Afficher la liste des utilisateurs
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_show', requirements: ['id' => '\d+'])]
+    public function show(int $id): Response
+    {
+        // Afficher les détails de l'utilisateur avec l'ID spécifié
+        return $this->render('user/show.html.twig', [
+            'controller_name' => 'UserController',
+            'user_id' => $id,
         ]);
     }
 
     #[Route('/edit', name: 'app_user_edit')]
     public function edit(): Response
     {
+        // Afficher le formulaire d'édition de l'utilisateur
         return $this->render('user/edit.html.twig', [
             'controller_name' => 'UserController',
         ]);
@@ -29,6 +40,7 @@ class UserController extends AbstractController
     #[Route('/auction', name: 'app_user_auction')]
     public function auction(): Response
     {
+        // Afficher les enchères de l'utilisateur
         return $this->render('user/auction.html.twig', [
             'controller_name' => 'UserController',
         ]);
@@ -37,9 +49,9 @@ class UserController extends AbstractController
     #[Route('/delete', name: 'app_user_delete')]
     public function delete(): Response
     {
+        //  supprimer l'utilisateur
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
-    
 }
