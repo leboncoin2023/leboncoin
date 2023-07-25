caf709363524b172a4b56fd433fab8c31041fbdb
Drahc0r <132657494+Drahc0r@users.noreply.github.com>
2023-07-25 17:24:59 +0200

    todo pop up

--- public/assets/js/toast.js
+++ public/assets/js/toast.js
@@ -3,3 +3,7 @@ let toastLiveExample = document.getElementById('liveToast')
 if (toastLiveExample) {
     toastLiveExample.classList.add('show');
 }
+
+
+
+//TODO: ajout de la possibilité de fermer le pop-up avec un addeventlistener et un display : none + refaire le html css du pop-up en suivant le modèle des ux/ui
