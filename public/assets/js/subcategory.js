7fa081ab261277863c2e7973e2b616eaedc60d1b
gazario <78473638+gazario@users.noreply.github.com>
2023-07-31 20:53:39 +0200

    JHJH

--- public/assets/js/subcategory.js
+++ public/assets/js/subcategory.js
@@ -1,5 +1,23 @@
 
 
-function getSubCategory(getSubCategory){
+document.addEventListener('DOMContentLoaded', (e) => {
+
+let category_selected=document.getElementById("cat_selected")
+
+function getSubCategory(category){
+
+    let subcategory="";
+
+    for (i=0;i<category.length;i++){
+        if (category[i]=="/"){
+            subcategory=category.substring(i+1,category.length-1);
+        }
+    }
+
+    
 
 }
+
+getAllCategoriesAndSub();
+
+});
