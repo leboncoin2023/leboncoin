db3fffcab138044179981f15fa6d5fe6f36f9e72
gazario <78473638+gazario@users.noreply.github.com>
2023-07-31 13:39:37 +0200

    correction map API pour nouvelle enchère

--- public/assets/js/script_maps_seller.js
+++ public/assets/js/script_maps_seller.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
             // récupération de la valeur saisie par l'utilisateur
             let localisation = e.target.value;
             getLocation(localisation);
-            //console.log('contenu adresse:',document.getElementById('address_input').innerText);
+            //console.log('contenu adresse:',localisation);
             
         });
     }
@@ -21,13 +21,13 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
     //initialisation des varibales
         let address_input = document.getElementById('address_input');
-        //let search_btn = document.getElementById('getMap');
+
         let maps_address = document.getElementById('maCarte');
         //let carte="";
 
-        //Modifier le libellé du bouton de validation du formulaire
-        let save_btn = document.getElementById('auction_save');
-        save_btn.innerText="Valider";
+            //Modifier le libellé du bouton de validation du formulaire
+            let save_btn = document.getElementById('auction_save');
+            save_btn.innerText="Valider";
  
     //Récupération des coordonnées GPS de l'adresse entrée par l'utilisateur
     function getLocation(location){
@@ -50,21 +50,34 @@ document.addEventListener('DOMContentLoaded', (e) => {
         
     // Affichage de la carte 
     function showMap(lat, lon){
+
+
+        // si container deja initialisé on efface le contenu       
+        var container = L.DomUtil.get('maCarte');
+        if(container != null){
+          container._leaflet_id = null;
+        }
+       
+       
+       
+
+            var carte = L.map('maCarte').setView([lat, lon], 18);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+                //lien vers la source des données
+                attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+                minZoom: 1,
+                maxZoom: 20
+            }).addTo(carte);
         
 
-        let carte = L.map('maCarte').setView([lat, lon], 18);
+        
+            L.marker([lat, lon]).addTo(carte)
+            .bindPopup('Votre bien se situe ici')
+            .openPopup();
 
-        L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-            //lien vers la source des données
-            attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-            minZoom: 1,
-            maxZoom: 20
-        }).addTo(carte);
-
-        //maps_address.innerHTML =`<i class="fa-solid fa-location-dot" style="color: #ff9200;"></i>`;
-        //let marker = L.marker(lat, lon).addTo(carte);
-        //marker.bindPopup("house");
-
+        
+    
     }
 
 
